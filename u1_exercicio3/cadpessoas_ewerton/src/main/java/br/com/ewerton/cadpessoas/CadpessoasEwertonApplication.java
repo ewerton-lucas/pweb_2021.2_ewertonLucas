f33cb82f5e2cf6a330c4f13d6d8c12face61e5cc
ewerton-lucas <ewertonfp28@gmail.com>
2022-03-12 10:04:38 -0300

    Exercício 3
    Exercício 3

---
+++ u1_exercicio3/cadpessoas_ewerton/src/main/java/br/com/ewerton/cadpessoas/CadpessoasEwertonApplication.java
@@ -0,0 +1,13 @@
+package br.com.ewerton.cadpessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadpessoasEwertonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadpessoasEwertonApplication.class, args);
+	}
+
+}
---
+++ u1_exercicio3/cadpessoas_ewerton/src/test/java/br/com/ewerton/cadpessoas/CadpessoasEwertonApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ewerton.cadpessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadpessoasEwertonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
