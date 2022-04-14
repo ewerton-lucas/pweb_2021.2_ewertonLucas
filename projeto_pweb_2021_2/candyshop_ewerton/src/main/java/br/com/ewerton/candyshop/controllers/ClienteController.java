b783af285ecf3b4a7875dc61f2aa7ff9316c47e7
ewerton-lucas <ewertonfp28@gmail.com>
2022-04-14 10:54:15 -0300

    Implementando MVC
    Modelo e Repositório do Cliente.

---
+++ projeto_pweb_2021_2/candyshop_ewerton/src/main/java/br/com/ewerton/candyshop/controllers/ClienteController.java
@@ -0,0 +1,5 @@
+package br.com.ewerton.candyshop.controllers;
+
+public class ClienteController {
+    
+}
---
+++ projeto_pweb_2021_2/candyshop_ewerton/src/main/java/br/com/ewerton/candyshop/models/Cliente.java
@@ -0,0 +1,95 @@
+package br.com.ewerton.candyshop.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name="clientes")
+
+public class Cliente implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String genero;
+
+    private String email;
+
+    private String cidade;
+
+    private String cep;
+    
+    private String telefone;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getGenero() {
+        return this.genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCidade() {
+        return this.cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getCep() {
+        return this.cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getTelefone() {
+        return this.telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+}
