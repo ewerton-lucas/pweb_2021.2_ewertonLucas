89731401019e89197a0cb6769f6709d312faf3ce
ewerton-lucas <ewertonfp28@gmail.com>
2022-04-13 22:00:46 -0300

    Enviando Projeto
    Loja de Doces - Pweb 2021.2

---
+++ projeto_pweb_2021_2/candyshop_ewerton/src/main/java/br/com/ewerton/candyshop/CandyshopEwertonApplication.java
@@ -0,0 +1,13 @@
+package br.com.ewerton.candyshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandyshopEwertonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandyshopEwertonApplication.class, args);
+	}
+
+}
