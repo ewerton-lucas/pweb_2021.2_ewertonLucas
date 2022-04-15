53333126b4c2d33777f21f9d92a6988fc31cf68d
ewerton-lucas <ewertonfp28@gmail.com>
2022-04-15 12:06:34 -0300

    Iniciando projeto

---
+++ diabeteshop_ewerton/src/main/java/br/com/ewerton/diabeteshop/DiabeteshopEwertonApplication.java
@@ -0,0 +1,13 @@
+package br.com.ewerton.diabeteshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiabeteshopEwertonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiabeteshopEwertonApplication.class, args);
+	}
+
+}
---
+++ diabeteshop_ewerton/src/test/java/br/com/ewerton/diabeteshop/DiabeteshopEwertonApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ewerton.diabeteshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiabeteshopEwertonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
