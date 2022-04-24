fb46cc4f0e374001d2d1134a34b02adf6ec7138a
ewerton-lucas <ewertonfp28@gmail.com>
2022-04-23 23:55:01 -0300

    Inserindo Layout
    - Layout base com fragments-thymeleaf;
    - Aplicando Bootstrap 5;
    - Nova dependência thymeleaf;
    - Personalizando layout com CSS;
    - Iniciando ClienteController.

--- diabeteshop_ewerton/src/main/java/br/com/ewerton/diabeteshop/controllers/ClienteController.java
+++ diabeteshop_ewerton/src/main/java/br/com/ewerton/diabeteshop/controllers/ClienteController.java
@@ -1,5 +1,15 @@
 package br.com.ewerton.diabeteshop.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ClienteController {
+
+    @GetMapping
+    public String index() {
+        return "index.html";
+    }
     
+
 }
